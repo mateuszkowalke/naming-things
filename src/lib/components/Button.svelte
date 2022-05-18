13333895c191043106e0d749276599af636be9ef
mateusz.kowalke <mateusz.kowalke@aurafutures.com>
2022-05-18 12:19:58 +0200

    up

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let id: string;
+	export let text: string;
+</script>
+
+<button {id}>{text}</button>
+
+<style>
+	button {
+		border: 0;
+		border-radius: 0.25rem;
+		background: #1e88e5;
+		color: white;
+		font-family: -system-ui, sans-serif;
+		font-size: 1.5rem;
+		line-height: 2;
+		white-space: nowrap;
+		text-decoration: none;
+		padding: 2rem 2rem;
+		margin: 0.25rem;
+		cursor: pointer;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,99 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { browser } from '$app/env';
+	import Button from '$lib/components/Button.svelte';
+
+	let error: string;
+	let result: boolean;
+	let riddle: { answers: string[]; imageUrl: string; imageAlt: string } = {
+		answers: ['auto', 'samochód'],
+		imageUrl: 'auto.png',
+		imageAlt: 'auto'
+	};
+
+	if (browser) {
+		/* @ts-ignore */
+		const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+		/* @ts-ignore */
+		const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+		/* @ts-ignore */
+		const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+		const grammar =
+			'#JSGF V1.0; grammar colors; public <color> = ' + riddle.answers.join(' | ') + ' ;';
+
+		const recognition = new SpeechRecognition();
+		const speechRecognitionList = new SpeechGrammarList();
+
+		speechRecognitionList.addFromString(grammar, 1);
+
+		recognition.grammars = speechRecognitionList;
+		recognition.continuous = false;
+		recognition.lang = 'pl';
+		recognition.interimResults = false;
+		recognition.maxAlternatives = 1;
+
+		document.getElementById('play')!.onclick = function () {
+			recognition.start();
+			result = false;
+			console.log('Ready to receive a color command.');
+		};
+
+		recognition.onresult = function (event: any) {
+			error = '';
+			console.log({ event });
+			for (let _res of event.results) {
+				for (let alt of event.results) {
+					if (riddle.answers.filter((item) => item === alt.transcript).length) {
+						result = true;
+						console.log('true')
+						return;
+					}
+				}
+			}
+		};
+
+		recognition.onspeechend = function () {
+			recognition.stop();
+		};
+
+		recognition.onnomatch = function (event: any) {
+			error = 'Nie rozpoznano wyrazu.';
+		};
+
+		recognition.onerror = function (event: any) {
+			console.log({ event });
+			error = event.error;
+			result = false;
+		};
+	}
+</script>
+
+<div>
+	<h1>Co to jest?</h1>
+	<div class="center">
+		<img src={riddle.imageUrl} alt={riddle.imageAlt} style:margin="auto" />
+	</div>
+	<div class="center">
+		<Button id="play" text="Naciśnij, aby spróbować" />
+	</div>
+	<div>
+		{#if result}
+			<h3>Brawo!!! Udało się!</h3>
+		{/if}
+		{#if error}
+			<p id="error"><em>Error: {error}</em></p>
+		{/if}
+	</div>
+</div>
+
+<style>
+	h1 {
+		display: block;
+		text-align: center;
+	}
+	div > .center {
+		display: block;
+		width: fit-content;
+		margin: auto;
+	}
+</style>
